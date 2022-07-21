13a764ce17acd39c94322f1d445c35c417e1dd90
eldalco <elkincortes03@gmail.com>
2022-07-21 18:03:10 -0500

    Migrated Component Form JS

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,45 @@
+import React, { useRef } from "react";
+import "./styles.css";
+
+interface Props {
+  setName: () => string;
+  setStatus: () => string;
+  setPage: () => number;
+
+}
+
+export default function Form({ setName, setStatus, setPage }:Props) {
+  const inputName = useRef<HTMLInputElement>(null);
+  const selectStatus = useRef<HTMLSelectElement>(null);
+  const onChangeSearch = (evt: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    evt.preventDefault();
+    const name = inputName.current?.value;
+    const status:string |undefined = selectStatus.current?.value;
+    setName(name);
+    setStatus(status);
+    setPage(1);
+  };
+  return (
+    <form className="c-form">
+      <input
+        ref={inputName}
+        onChange={onChangeSearch}
+        className="form__input"
+        type="text"
+        placeholder="Character's name..."
+      />
+      <select
+        ref={selectStatus}
+        onChange={onChangeSearch}
+        className="form__select"
+        name="select-status"
+      >
+        <option value="" disabled selected="selected">Select status</option>
+        <option value="">All</option>
+        <option value="Alive">ALIVE</option>
+        <option value="Dead">DEAD</option>
+        <option value="unknown">UNKNOWN</option>
+      </select>
+    </form>
+  );
+}
