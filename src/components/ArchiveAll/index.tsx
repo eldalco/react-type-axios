1b955effccb89d8ec9004dfa95e960c4f7df2f0a
eldalco <elkincortes03@gmail.com>
2022-07-21 18:01:28 -0500

    Migrated Component AdvancedSearch JS

--- src/components/ArchiveAll/index.tsx
+++ src/components/ArchiveAll/index.tsx
@@ -1,17 +1,32 @@
-import {useEffect} from 'react'
-import useGetAllCharacters from '../../hooks/useGetAllCharacters'
+import { useEffect, useState } from "react";
+import useGetAllCharacters from "../../Hooks/useGetAllCharacters";
 
-export const ArchiveAll = (): JSX.Element => {
-    const {isLoadingGet, hasErrorGet, dataGet, successGet, get}= useGetAllCharacters();
-    useEffect (()=> {
-        get()
-    },[])
-    useEffect (()=> {
-        dataGet && console.log(dataGet)
-    },[dataGet])
-    
-    return (
-        <div>ArchiveClans</div>
-        
-    )
+import AdvancedSearch from "../AdvancedSearch";
+import Characters from "../Characters";
+import Loading from "../Loading";
+import Paginated from "../Paginated";
+import "./styles.css";
+
+export default function ArchiveCharacters() {
+  const [name, setName] = useState("");
+  const [status, setStatus] = useState("");
+  const [page, setPage] = useState(1);
+  const { isLoadingGet, hasErrorGet, dataGet, get } =
+  useGetAllCharacters();
+
+  useEffect(() => {
+    get(page, name, status);
+  }, [get, page, name, status]);
+
+  return (
+    <div className="c-archive-characters">
+      <AdvancedSearch setName={setName} setStatus={setStatus} setPage={setPage} />
+      {!isLoadingGet && <Characters characters={dataGet?.results} />}
+      {isLoadingGet && <Loading />}
+      {!isLoadingGet && (
+        <Paginated infoPage={dataGet?.info} page={page} setPage={setPage} />
+      )}
+      {/* {hasErrorGet && <Error />} */}
+    </div>
+  );
 }
