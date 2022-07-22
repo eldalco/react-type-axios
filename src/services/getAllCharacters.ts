351bbf5dfda26f08487a6641652cbb92d3ed3af0
eldalco <elkincortes03@gmail.com>
2022-07-22 11:19:53 -0500

    Deleted comments in service getAllCHaracters

--- src/services/getAllCharacters.ts
+++ src/services/getAllCharacters.ts
@@ -1,21 +1,13 @@
 import axios from "axios";
+import {GetDataResponse} from "../types"
 
-// const baseUrl:string = "https://api.clashofclans.com/v1/clans/";
-// const token:string = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImIxZjI2ZmQ0LTk5MmMtNGY3OC1hOWM2LTE0YWVkZDU5YjlkZiIsImlhdCI6MTY1ODE5MjY1Niwic3ViIjoiZGV2ZWxvcGVyLzc2NzJiNzBiLWEzYjUtZGQ5ZC01NjZmLTUzODYwZTY1OWM1NCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjQ1LjE3My40LjIxMCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.WMdjqTrMhAMKN9BRx_RwK9mNCZDs4IxNeQlvfykdGWCq6AfGK2sYCr1ocio1q1KUHmglon02pzrN9VTVDjuF2A";
 const baseUrl:string = "https://rickandmortyapi.com/api/character";
 
-type GetResponse = {
-  data: [];
-};
 
 const get = (page:number=1, name:string="", status:string="") => {
-    // const config = {
-    //   headers: {
-    //     Authorization: `Bearer ${token}`,
-    //   },
-    // };
-    const request = axios.get<GetResponse>(`${baseUrl}?page=${page}&name=${name}&status=${status}`);
-    return request.then((response) => response);
+
+    const request = axios.get<GetDataResponse>(`${baseUrl}?page=${page}&name=${name}&status=${status}`);
+    return request.then((response) => response.data);
 };
   
 
