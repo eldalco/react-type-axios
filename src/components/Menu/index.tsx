14a5eacd8dd9c3419952237edfaaac36bffe54de
eldalco <elkincortes03@gmail.com>
2022-07-30 18:55:07 -0500

    Settings in structure of menu an styles

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,23 +1,22 @@
 import "./styles.css";
+import { FaGithubSquare } from 'react-icons/fa';
 
 export default function Menu():JSX.Element {
   return (
     <section className="c-menu">
       <img
-        height="96px"
-        width="315px"
         className="menu__logo"
-        src="assets/images/rick-and-morty-logo.webp"
+        src="assets/images/RaM-logo.webp"
         alt="Logo Rick And Morty"
       />
       <nav className="menu__items">
         <a
           className="menu__item"
-          href="https://github.com/dalfonso75/rick-and-morty"
+          href="https://github.com/eldalco/react-type-axios"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Github
+          <FaGithubSquare size={50}/>
         </a>
       </nav>
     </section>
