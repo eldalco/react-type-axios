2ef9fbfd2e2414f723622c0700e0567f4b59b9b2
eldalco <elkincortes03@gmail.com>
2022-07-30 19:18:58 -0500

    footer customized

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,28 +1,22 @@
 import "./styles.css";
+import { FaGithubSquare } from 'react-icons/fa';
 
 export default function Footer() {
   return (
     <div className="c-footer">
-      <img
-        className="footer__image"
-        src="assets/images/morty.webp"
-        alt="Morty"
-      />
       <div className="footer__content">
         <a
           className="menu__item"
-          href="https://github.com/dalfonso75/rick-and-morty"
+          href="https://github.com/eldalco/react-type-axios"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Github
+          <FaGithubSquare size={50}/>
         </a>
-        <p className="footer__copyright">
-          Technical test carried out by Deiver Alfonso for Toughbuilt. License
-          MIT
-        </p>
       </div>
-      <img className="footer__image" src="assets/images/rick.webp" alt="Rick" />
+      <div className="box__image">
+        <img className="image__footer" src="assets/images/portal-weapon.webp" alt="Rick" />
+      </div>
     </div>
   );
 }
